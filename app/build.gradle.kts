e61e5b1f804e6f728609bd79bc4f90f9827e8fa1
shenriquezneira <shenriquezneira@gmail.com>
2026-02-18 07:32:33 -0300

    Pantalla Principal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,9 @@ android {
         version = release(36)
     }
 
+    buildFeatures {
+        viewBinding = true;
+    }
     defaultConfig {
         applicationId = "com.example.evaluacionm4"
         minSdk = 26
@@ -17,6 +20,7 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
 
     buildTypes {
         release {
@@ -41,4 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.11.0")
+
 }
--- app/src/main/java/com/example/evaluacionm4/MainActivity.java
+++ app/src/main/java/com/example/evaluacionm4/MainActivity.java
@@ -8,17 +8,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.evaluacionm4.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(activityMainBinding.getRoot());
+
+        //activityMainBinding.textViewSaludo.setText("Hola mundo");
     }
 }
